5d4fecbb3c7d86ee5db62730e5503047597323d7
oas1s <azatazat835@mail.ru>
2024-09-10 17:10:43 +0300

    add lesson 1

---
+++ 01. Java Intro/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String name = scanner.nextLine();
+//        System.out.println("Hello world, " + name + "!");
+
+//        // long,int,byte
+//        int a = 1;
+//        int b = 2;
+//        int c = a+b;
+//        System.out.println(c);
+//
+//        // double, float
+//        double d = 1.1;
+//        double e = 1.1;
+//        System.out.println(d-e);
+
+//        // arrays
+//        int[] arr = {1,2,3};
+//        int[] arr1 = new int[3];
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println(arr1[i]);
+//        }
+
+//        int[] arr = {2,1,3,4,2};
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] >= 2){
+//                System.out.println(arr[i]);
+//            }
+//        }
+
+//        // logic operations
+//        boolean flag = true;
+//        int[] arr = {2,1,3,4,2};
+//        for (int i = 0; i < arr.length; i++) {
+//            if (flag){
+//                System.out.println(arr[i]);
+//            }
+//        }
+
+//        // NPE
+//        String s = null;
+//        System.out.println(s.equals(" "));
+
+    }
+}
