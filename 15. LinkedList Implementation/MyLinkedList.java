0e982c55dd1f72e399529638b289aeeb90ae2743
oas1s <azatazat835@mail.ru>
2025-02-08 10:59:25 +0300

    lesson 15 add

---
+++ 15. LinkedList Implementation/MyLinkedList.java
@@ -0,0 +1,144 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MyLinkedList implements List<Integer> {
+
+    private Node head = null;
+    private Node tail = null;
+
+     private class Node {
+        Integer value;
+        Node next;
+
+        public Node(Integer value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(Integer integer) {
+        if (head == null){
+            head = new Node(integer,null);
+            tail = head;
+        } else {
+            Node node = new Node(integer,null);
+            tail.next = node;
+            tail = node;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Integer> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Integer> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Integer get(int index) {
+        return 0;
+    }
+
+    @Override
+    public Integer set(int index, Integer element) {
+        return 0;
+    }
+
+    @Override
+    public void add(int index, Integer element) {
+
+    }
+
+    @Override
+    public Integer remove(int index) {
+        return 0;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<Integer> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<Integer> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<Integer> subList(int fromIndex, int toIndex) {
+        return List.of();
+    }
+}
